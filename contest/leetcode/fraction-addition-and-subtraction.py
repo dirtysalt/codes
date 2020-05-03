fd40e23ae35a2654684da1caf38e81f4fb849bd9
yanz <dirtysalt1987@gmail.com>
2020-05-03 21:12:07 +0800

    fix

--- contest/leetcode/fraction-addition-and-subtraction.py
+++ contest/leetcode/fraction-addition-and-subtraction.py
@@ -4,50 +4,37 @@
 
 class Solution:
     def fractionAddition(self, expression: str) -> str:
-        ans = None
-
-        def add(op1, op2):
-            a, b = op1
-            c, d = op2
-            x = a * d + b * c
-            y = b * d
-            x, y = norm(x, y)
-            return x, y
 
         def gcd(x, y):
             while y != 0:
                 x, y = y, x % y
             return x
 
-        def norm(x, y):
-            g = gcd(x, y)
-            return x // g, y // g
+        def add(x, y, a, b):
+            xx = b * x + a * y
+            yy = y * b
+            _gcd = gcd(xx, yy)
+            return xx // _gcd, yy // _gcd
+
+        def parse_int(expr, i):
+            v = 0
+            while i < len(expr) and expr[i].isdigit():
+                v = v * 10 + int(expr[i])
+                i += 1
+            return v, i
 
         i = 0
-        expr = expression
-        while i < len(expr):
+        x, y = 0, 1
+
+        while i < len(expression):
             sign = 1
-            if expr[i] in '+-':
-                if expr[i] == '-':
+            if expression[i] in '+-':
+                if expression[i] == '-':
                     sign = -1
                 i += 1
 
-            a = 0
-            while i < len(expr) and expr[i].isdigit():
-                a = a * 10 + (ord(expr[i]) - ord('0'))
-                i += 1
+            a, i = parse_int(expression, i)
+            b, i = parse_int(expression, i + 1)
             a = a * sign
-
-            i += 1
-            b = 0
-            while i < len(expr) and expr[i].isdigit():
-                b = b * 10 + (ord(expr[i]) - ord('0'))
-                i += 1
-
-            if ans is None:
-                ans = norm(a, b)
-            else:
-                ans = add(ans, (a, b))
-
-        a, b = ans
-        return '%d/%d' % (a, b)
+            x, y = add(x, y, a, b)
+        return "{}/{}".format(x, y)
