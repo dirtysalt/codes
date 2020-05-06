f4c8e7eaa83105c2e37b10ae55c8cc1a77117ea9
yanz <dirtysalt1987@gmail.com>
2020-05-06 17:09:01 +0800

    add solutions

--- contest/leetcode/decode-ways.py
+++ contest/leetcode/decode-ways.py
@@ -2,37 +2,34 @@
 # coding:utf-8
 # Copyright (C) dirlt
 
-class Solution(object):
-    def numDecodings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = {}
 
-        # preprocess.
-        if not s: return 0
-        if not s.isdigit(): return 0
+        def fun(i):
+            if i == len(s):
+                return 1
+            if s[i] == '0':
+                return 0
+            if i in dp:
+                return dp[i]
 
-        st = [0] * (len(s) + 1)
-        st[0] = 1
+            ans = 0
+            if s[i] == '1':
+                # try 1 and 10-19.
+                ans = fun(i + 1)
+                if (i + 1) < len(s):
+                    ans += fun(i + 2)
+            elif s[i] == '2':
+                # try 2 and 20-26
+                ans = fun(i + 1)
+                if (i + 1) < len(s) and s[i + 1] in '0123456':
+                    ans += fun(i + 2)
+            else:  # 3-9
+                ans = fun(i + 1)
 
-        for i in range(0, len(s)):
-            res = 0
-            v = int(s[i])
-            if v >= 1 and v <= 26:
-                res += st[i]
+            dp[i] = ans
+            return ans
 
-            if i > 0:
-                v = int(s[i - 1: i + 1])
-                if s[i - 1] != '0' and 1 <= v <= 26:
-                    res += st[i - 1]
-
-            st[i + 1] = res
-
-        return st[len(s)]
-
-
-if __name__ == '__main__':
-    s = Solution()
-    print(s.numDecodings('12'))
-    print(s.numDecodings('27'))
+        ans = fun(0)
+        return ans
