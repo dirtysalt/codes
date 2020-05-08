43fe6b638f7748f5e25950dfb8f497bf3b56b7a9
yanz <dirtysalt1987@gmail.com>
2020-05-08 16:26:02 +0800

    add spring-web

--- java/spring-web/src/main/java/com/example/demo/DemoController.java
+++ java/spring-web/src/main/java/com/example/demo/DemoController.java
@@ -4,10 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class DemoController {
-    @GetMapping("/hello")
+    final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello")`
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format("Hello %s!", name));
     }
 }
---
+++ java/spring-web/src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class Greeting {
+    final long id;
+    final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
