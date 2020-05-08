174a3d30bf66ad9b0399c12109df9f1368161088
yanz <dirtysalt1987@gmail.com>
2020-05-08 20:22:38 +0800

    fix spring-web. support mongodb

--- java/spring-web/src/main/java/com/example/demo/DemoApplication.java
+++ java/spring-web/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +17,10 @@ import org.springframework.core.env.Environment;
 public class DemoApplication {
     @Autowired
     Environment env;
+    @Autowired
+    MongoDBService mongodb;
+
+    protected static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -21,6 +30,13 @@ public class DemoApplication {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
             System.out.printf("[env]running on port %s\n", env.getProperty("local.server.port"));
+
+            MongoDatabase testdb = mongodb.getDatabase("test");
+            MongoCollection<Document> coll = testdb.getCollection("spring", Document.class);
+            Document doc = new Document();
+            doc.append("name", "yan");
+            doc.append("age", 10);
+            coll.insertOne(doc);
 
 //            System.out.println("Let's inspect the beans provided by Spring Boot:");
 //
--- java/spring-web/src/main/java/com/example/demo/DemoController.java
+++ java/spring-web/src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,9 +12,11 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class DemoController {
     final AtomicLong counter = new AtomicLong();
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.info("hello request with name = " + name);
         return String.format("Hello %s!", name);
     }
 
