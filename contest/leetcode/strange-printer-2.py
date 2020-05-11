4455dae09d0949125cc809e79cad04463ed45014
yanz <dirtysalt1987@gmail.com>
2020-05-11 22:30:28 +0800

    hard to explain why it's correct.

--- contest/leetcode/strange-printer-2.py
+++ contest/leetcode/strange-printer-2.py
@@ -4,37 +4,39 @@
 
 class Solution:
     def strangePrinter(self, s: str) -> int:
-        dp = {}
+        if not s:
+            return 0
 
+        p = s[0]
+        ss = []
+        for c in s:
+            if c != p:
+                ss.append(p)
+                p = c
+        ss.append(p)
+        print(''.join(ss))
+
+        import functools
+
+        @functools.lru_cache(None)
         def fun(i, j):
             if i > j:
                 return 0
-            if i == j:
-                return 1
 
-            if (i, j) in dp:
-                return dp[(i, j)]
-
-            ans = 1 + fun(i + 1, j)  # print once char.
-
+            ans = 1 + fun(i + 1, j)
             for k in range(i + 1, j + 1):
-                if s[i] == s[k]:
-                    # print i..k
-                    # then print i+1..k and k+1..j
+                if ss[i] == ss[k]:
                     ans = min(ans, fun(i + 1, k) + fun(k + 1, j))
-
-            dp[(i, j)] = ans
             return ans
 
-        ans = fun(0, len(s) - 1)
+        ans = fun(0, len(ss) - 1)
         return ans
 
 
 cases = [
     ('aaabbb', 2),
     ('aba', 2),
-    ("bacdadacbdbcabdabdbcdcbacbdcabca", 19),
-    ('bacdada', 5),
+    ("baacdddaaddaaaaccbddbcabdaabdbbcdcbbbacbddcabcaaa", 19),
     ('abaca', 3)
 ]
 
