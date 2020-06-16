1d88c9a525a7cfcfc32b108c2e6cd543475847f7
yanz <dirtysalt1987@gmail.com>
2020-06-16 19:38:29 +0800

    add solutions

--- contest/hackerrank/almost-sorted.py
+++ contest/hackerrank/almost-sorted.py
@@ -1,41 +1,44 @@
-#!/usr/bin/env python
-# coding:utf-8
-# Copyright (C) dirlt
-
-# !/bin/python3
-
+#!/bin/python3
 
 # Complete the almostSorted function below.
 def almostSorted(arr):
-    xs = [(arr[i], i) for i in range(len(arr))]
-    xs.sort()
-
+    n = len(arr)
+    xs = [(i, arr[i]) for i in range(n)]
+    xs.sort(key=lambda x: x[1])
     st = []
-    for i in range(len(xs)):
-        if xs[i][1] != i:
-            st.append(xs[i][1])
+    for i in range(n):
+        if xs[i][0] != i:
+            st.append(xs[i][0])
 
     if len(st) == 0:
         print('yes')
+        return
 
-    elif len(st) == 2:
-        print('yes')
-        print('swap %d %d' % (st[-1] + 1, st[0] + 1))
+    assert len(st) > 1
+    ok = True
+    for i in range(1, len(st)):
+        if st[i - 1] - st[i] < 0:
+            ok = False
+            break
+    if not ok:
+        print('no')
+        return
 
+    print('yes')
+    a = st[-1] + 1
+    b = st[0] + 1
+    if len(st) == 2:
+        print('swap %d %d' % (a, b))
     else:
-        ok = True
-        for i in range(1, len(st)):
-            if (st[i - 1] - st[i]) != 1:
-                ok = False
-                break
-        if ok:
-            print('yes')
-            print('reverse %d %d' % (st[-1] + 1, st[0] + 1))
-        else:
-            print('no')
+        print('reverse %d %d' % (a, b))
 
 
 if __name__ == '__main__':
+    import os
+    import sys
+
+    if os.path.exists('tmp.in'):
+        sys.stdin = open('tmp.in')
     n = int(input())
 
     arr = list(map(int, input().rstrip().split()))
