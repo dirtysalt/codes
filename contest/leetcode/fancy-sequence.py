02a650dac96ae41f1ca245f7c964048554f6c313
zhyanzy <zhyanzy@amazon.com>
2020-10-19 17:22:26 +0800

    add bi37 solutions(fix P3 and P4)

--- contest/leetcode/fancy-sequence.py
+++ contest/leetcode/fancy-sequence.py
@@ -3,30 +3,44 @@
 # Copyright (C) dirlt
 
 
-class Fancy:
+MOD = 10 ** 9 + 7
 
+
+def POW(a, b):
+    ans = 1
+    a = a % MOD
+    while b:
+        if b & 0x1:
+            ans = ans * a
+            ans = ans % MOD
+        b = b >> 1
+        a = (a * a) % MOD
+    return ans
+
+
+class Fancy:
     def __init__(self):
         self.array = []
         self.ops = []
-        self.cache = {}
+        self.ops.append((-1, 0, 1, 1))
 
     def append(self, val: int) -> None:
         self.array.append(val)
 
     def addAll(self, inc: int) -> None:
         idx = len(self.array) - 1
-        self.cache.clear()
-        self.ops.append((idx, 0, inc))
+        _, add, mul, _ = self.ops[-1]
+        self.ops.append((idx, add + inc, mul, POW(mul, MOD - 2)))
 
     def multAll(self, m: int) -> None:
         idx = len(self.array) - 1
-        self.cache.clear()
-        self.ops.append((idx, 1, m))
+        _, add, mul, _ = self.ops[-1]
+        self.ops.append((idx, add * m, (mul * m) % MOD, POW(mul * m, MOD - 2)))
 
     def getIndex(self, idx: int) -> int:
         if idx >= len(self.array):
             return -1
-        MOD = 10 ** 9 + 7
+
         s, e = 0, len(self.ops) - 1
         while s <= e:
             m = (s + e) // 2
@@ -35,22 +49,25 @@ class Fancy:
             else:
                 s = m + 1
 
-        # starts with s
-        if s in self.cache:
-            add, mul = self.cache[s]
-        else:
-            add, mul = 0, 1
-            for _, op, v in self.ops[s:]:
-                if op == 0:
-                    add += v
-                else:
-                    add *= v
-                    mul *= v
-            # print(add, mul)
-            self.cache[s] = (add, mul)
+        # print(self.ops)
+        # apply latest op
+        # and cancel op self.ops[e]
 
-        val = self.array[idx] * mul + add
-        return val % MOD
+        _, add1, mul1, div1 = self.ops[-1]
+        _, add2, mul2, div2 = self.ops[e]
+        val = self.array[idx]
+
+        # mul = mul1 // mul2
+        # add = add1 - add2 * mul
+        # ans = val * mul + add
+
+        mul = (mul1 * div2) % MOD
+        add = add1 - add2 * mul
+        ans = val * mul + add
+        while ans < 0:
+            ans += MOD
+        return ans % MOD
+
 
 # Your Fancy object will be instantiated and called as such:
 # obj = Fancy()
@@ -58,3 +75,16 @@ class Fancy:
 # obj.addAll(inc)
 # obj.multAll(m)
 # param_4 = obj.getIndex(idx)
+
+null = None
+cases = [
+    (["Fancy", "append", "addAll", "append", "multAll", "getIndex", "addAll", "append", "multAll", "getIndex",
+      "getIndex", "getIndex"],
+     [[], [2], [3], [7], [2], [0], [3], [10], [2], [0], [1], [2]],
+     [null, null, null, null, null, 10, null, null, null, 26, 34, 20]),
+
+]
+
+import aatest_helper
+
+aatest_helper.run_simulation_cases(Fancy, cases)
--- contest/leetcode/number-of-sets-of-k-non-overlapping-line-segments.py
+++ contest/leetcode/number-of-sets-of-k-non-overlapping-line-segments.py
@@ -27,15 +27,60 @@ class Solution:
         return ans
 
 
+class Solution2:
+    def numberOfSets(self, n: int, k: int) -> int:
+        MOD = 10 ** 9 + 7
+        dp = [[0] * n for _ in range(k + 1)]
+
+        # dp[k][x] 以 x结束，可以分为k段的组合数
+        for i in range(n):
+            dp[1][i] = i
+
+        for kk in range(2, k + 1):
+            acc = 0
+            for i in range(n - 1):
+                acc += dp[kk - 1][i]
+                dp[kk][i + 1] = dp[kk][i] + acc
+
+        ans = 0
+        for i in range(n):
+            ans += dp[k][i]
+        return ans % MOD
+
+
+class Solution3:
+    def numberOfSets(self, n: int, k: int) -> int:
+        MOD = 10 ** 9 + 7
+        dp = [[0] * n for _ in range(2)]
+
+        # dp[k][x] 以x结束，可以分为k段的组合数
+        for i in range(n):
+            dp[0][i] = i
+
+        now = 0
+        for kk in range(2, k + 1):
+            to = 1 - now
+            acc = 0
+            for i in range(n - 1):
+                acc += dp[now][i]
+                dp[to][i + 1] = dp[to][i] + acc
+            now = to
+
+        ans = 0
+        for i in range(n):
+            ans += dp[now][i]
+        return ans % MOD
+
+
 cases = [
     (4, 2, 5),
     (3, 1, 3,),
     (30, 7, 796297179),
     (5, 3, 7),
     (3, 2, 1),
-    # (1000, 999, 1),
+    (1000, 999, 1),
 ]
 
 import aatest_helper
 
-aatest_helper.run_test_cases(Solution().numberOfSets, cases)
+aatest_helper.run_test_cases(Solution3().numberOfSets, cases)
