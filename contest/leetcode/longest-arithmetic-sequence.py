cb1e98240e0808207eaed22f61cd0fd44f43aab7
zhyanzy <zhyanzy@amazon.com>
2020-11-20 12:43:04 +0800

    update

--- contest/leetcode/longest-arithmetic-sequence.py
+++ contest/leetcode/longest-arithmetic-sequence.py
@@ -7,58 +7,35 @@ from typing import List
 
 class Solution:
     def longestArithSeqLength(self, A: List[int]) -> int:
-        from collections import defaultdict
-        nums = defaultdict(list)
         n = len(A)
-
-        for i in range(n):
-            x = A[i]
-            nums[x].append(i)
-
-        def find_next(exp, i):
-            xs = nums[exp]
-            s, e = 0, len(xs) - 1
-            while s <= e:
-                m = (s + e) // 2
-                if xs[m] > i:
-                    e = m - 1
-                else:
-                    s = m + 1
-            # print(exp, xs, i, s)
-            if s >= len(xs):
-                return None
-            return xs[s]
-
-        visited = set()
-        ans = 2
-        for i in range(n):
-            for j in range(i + 1, n):
-                d = A[j] - A[i]
-                if (i, j) in visited:
-                    continue
-                visited.add((i, d))
-                sz = 2
-                exp = A[j] + d
-                k = j
-                while True:
-                    k2 = find_next(exp, k)
-                    if k2 is None:
-                        break
-                    visited.add((k, k2))
-                    sz += 1
-                    exp += d
-                    k = k2
-                ans = max(ans, sz)
+        # dp[i][d] # 以A[i]截止，差距为d的最长序列长度
+        dp = [{}]
+        ans = 0
+        for i in range(1, n):
+            dp.append({})
+            for j in range(i):
+                d = A[i] - A[j]
+                value = dp[j].get(d, 1) + 1
+                dp[i][d] = value
+                ans = max(ans, value)
         return ans
 
 
 cases = [
     (
         [44, 46, 22, 68, 45, 66, 43, 9, 37, 30, 50, 67, 32, 47, 44, 11, 15, 4, 11, 6, 20, 64, 54, 54, 61, 63, 23, 43, 3,
-         12,
-         51, 61, 16, 57, 14, 12, 55, 17, 18, 25, 19, 28, 45, 56, 29, 39, 52, 8, 1, 21, 17, 21, 23, 70, 51, 61, 21, 52,
-         25,
-         28], 6)
+         12, 51, 61, 16, 57, 14, 12, 55, 17, 18, 25, 19, 28, 45, 56, 29, 39, 52, 8, 1, 21, 17, 21, 23, 70, 51, 61, 21,
+         52,
+         25, 28], 6),
+    (
+        [61, 28, 67, 53, 13, 6, 70, 5, 79, 82, 60, 60, 84, 17, 80, 25, 82, 82, 69, 76, 81, 43, 58, 86, 18, 78, 4, 25, 8,
+         30,
+         33, 87, 91, 18, 90, 26, 62, 11, 28, 66, 9, 33, 58, 66, 47, 48, 80, 38, 25, 57, 4, 84, 79, 71, 54, 84, 63, 32,
+         97,
+         62, 26, 68, 5, 69, 54, 93, 25, 26, 100, 73, 24, 94, 80, 39, 30, 45, 95, 80, 0, 29, 57, 98, 92, 15, 17, 76, 69,
+         11,
+         57, 56, 48, 10, 28, 7, 63, 66, 53, 58, 12, 58], 6)
+
 ]
 
 import aatest_helper
