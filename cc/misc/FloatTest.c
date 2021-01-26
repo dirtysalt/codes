f69d0436adff39c3d74b7888231d46c8e6176ffc
yanz <dirtysalt1987@gmail.com>
2021-01-26 21:21:10 +0800

    update & format

--- cc/misc/FloatTest.c
+++ cc/misc/FloatTest.c
@@ -2,15 +2,15 @@
  * Copyright (C) dirlt
  */
 
-#include <stdio.h>
-#include <math.h>
 #include <assert.h>
+#include <math.h>
+#include <stdio.h>
 
 union Value {
     struct Float {
-        unsigned int base: 23;
-        unsigned int exp: 8;
-        unsigned int sign: 1;
+        unsigned int base : 23;
+        unsigned int exp : 8;
+        unsigned int sign : 1;
     } p;
     float f;
     unsigned int i;
@@ -19,27 +19,29 @@ union Value {
 long _pow2(unsigned int x) {
     long res = 1;
     long base = 2;
-    while(x) {
-        if(x & 0x1) res *= base;
+    while (x) {
+        if (x & 0x1) res *= base;
         base = base * base;
         x = x >> 1;
     }
     return res;
 }
-float pow2(int x){
+float pow2(int x) {
     unsigned int _x = x;
-    if(x < 0) _x = -x;
+    if (x < 0) _x = -x;
     long res = _pow2(_x);
-    if(x < 0) return 1.0f / res;
-    else return 1.0f * res;
+    if (x < 0)
+        return 1.0f / res;
+    else
+        return 1.0f * res;
 }
 
 void testSyn(float f) {
     union Value value;
     value.f = f;
     printf("=============testSyn==============\n");
-    printf("float = %.6f, int = %d, parts = (%d,%d,%d)\n", value.f,
-           value.i, value.p.sign, value.p.exp, value.p.base);
+    printf("float = %.6f, int = %d, parts = (%d,%d,%d)\n", value.f, value.i,
+           value.p.sign, value.p.exp, value.p.base);
     float res = 0.0f;
     if (!(value.p.exp == 0 && value.p.base == 0)) {
         float base = 1 + value.p.base * 1.0f / (1 << 23);
@@ -58,7 +60,7 @@ void testOp(float f) {
     value.f = f;
     // flip sign bit.
     value.i = value.i ^ 0x80000000;
-    assert(fabs(value.f + f) <1e-6);
+    assert(fabs(value.f + f) < 1e-6);
 
     // multiple 4
     const int scale = 2;
@@ -67,17 +69,16 @@ void testOp(float f) {
     assert(fabs(value.f - (1 << scale) * f) < 1e-6);
 }
 
-
 /*
-	.loc 1 72 0
-	movl	$-1, -36(%rbp)
-	.loc 1 73 0
-	movl	-36(%rbp), %eax
-	movl	%eax, %edx
-	shrl	$31, %edx
-	addl	%edx, %eax
-	sarl	%eax
-	movl	%eax, -36(%rbp)
+        .loc 1 72 0
+        movl	$-1, -36(%rbp)
+        .loc 1 73 0
+        movl	-36(%rbp), %eax
+        movl	%eax, %edx
+        shrl	$31, %edx
+        addl	%edx, %eax
+        sarl	%eax
+        movl	%eax, -36(%rbp)
 */
 
 int main() {
@@ -86,7 +87,7 @@ int main() {
     printf("%d\n", x);
     float data[] = {0.15625f, 15622.76f, 0.0f};
     unsigned int size = sizeof(data) / sizeof(data[0]);
-    for(unsigned int i=0;i<size;i++) {
+    for (unsigned int i = 0; i < size; i++) {
         float f = data[i];
         testSyn(f);
         testSyn(-f);
--- cc/misc/PopCountTest.cpp
+++ cc/misc/PopCountTest.cpp
@@ -2,66 +2,63 @@
  * Copyright (C) dirlt
  */
 
-// CppApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// CppApp.cpp : This file contains the 'main' function. Program execution begins
+// and ends there.
 //
 
-#include <vector>
-#include <iostream>
-#include <chrono>
-#include <cassert>
-#include <stdint.h>
 #include <immintrin.h>
-#define min(a, b) ((a) < (b)?(a): (b))
+#include <stdint.h>
 
-class Timer
-{
-    public:
-	void start()
-	{
-		m_StartTime = std::chrono::system_clock::now();
-		m_bRunning = true;
-	}
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <vector>
+#define min(a, b) ((a) < (b) ? (a) : (b))
 
-	void stop()
-	{
-		m_EndTime = std::chrono::system_clock::now();
-		m_bRunning = false;
-	}
+class Timer {
+   public:
+    void start() {
+        m_StartTime = std::chrono::system_clock::now();
+        m_bRunning = true;
+    }
 
-	long long  elapsedMilliseconds()
-	{
-		std::chrono::time_point<std::chrono::system_clock> endTime;
+    void stop() {
+        m_EndTime = std::chrono::system_clock::now();
+        m_bRunning = false;
+    }
 
-		if (m_bRunning)
-            {
-                endTime = std::chrono::system_clock::now();
-            }
-		else
-            {
-                endTime = m_EndTime;
-            }
+    long long elapsedMilliseconds() {
+        std::chrono::time_point<std::chrono::system_clock> endTime;
 
-		return std::chrono::duration_cast<std::chrono::milliseconds>(endTime - m_StartTime).count();
-	}
+        if (m_bRunning) {
+            endTime = std::chrono::system_clock::now();
+        } else {
+            endTime = m_EndTime;
+        }
 
-	double elapsedSeconds()
-	{
-		return elapsedMilliseconds() / 1000.0;
-	}
+        return std::chrono::duration_cast<std::chrono::milliseconds>(
+                   endTime - m_StartTime)
+            .count();
+    }
 
-    private:
-	std::chrono::time_point<std::chrono::system_clock> m_StartTime;
-	std::chrono::time_point<std::chrono::system_clock> m_EndTime;
-	bool                                               m_bRunning = false;
+    double elapsedSeconds() { return elapsedMilliseconds() / 1000.0; }
+
+   private:
+    std::chrono::time_point<std::chrono::system_clock> m_StartTime;
+    std::chrono::time_point<std::chrono::system_clock> m_EndTime;
+    bool m_bRunning = false;
 };
 
 typedef unsigned int uint32_t;
 
 std::vector<uint32_t> GenRandomInts(int N) {
-    srand(time(NULL));   // Initialization, should only be called once.
+    srand(time(NULL));  // Initialization, should only be called once.
     std::vector<uint32_t> ans(N);
-    for(int i=0;i<N;i++) {
-        int r = rand() % (1 << 30);      // Returns a pseudo-random integer between 0 and RAND_MAX.
+    for (int i = 0; i < N; i++) {
+        int r =
+            rand() %
+            (1
+             << 30);  // Returns a pseudo-random integer between 0 and RAND_MAX.
         ans[i] = (uint32_t)r;
     }
     return ans;
@@ -69,7 +66,7 @@ std::vector<uint32_t> GenRandomInts(int N) {
 
 uint32_t popcount11(uint32_t x) {
     uint32_t ans = 0;
-    while(x) {
+    while (x) {
         ans += x & 0x1;
         x = x >> 1;
     }
@@ -121,22 +118,21 @@ uint32_t popcount24(uint32_t x, uint32_t y, uint32_t a, uint32_t b) {
     return x;
 }
 
+#define FORI(i, t) for (size_t i = 0; i < t; i++)
+#define FORV(i, vec, step) for (size_t i = 0; i < vec.size(); i += step)
 
-#define FORI(i,t) for(size_t i=0;i<t;i++)
-#define FORV(i,vec,step) for(size_t i=0;i<vec.size();i+=step)
-
+#ifdef USE_AVX512_POPCNT
 // don't use it. I don't have any cpu support avx512 vpopcnt dq.
 // https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html
 // g++ mm.cpp -g -W -Wall -mavx512f -mavx512vpopcntdq
 uint32_t avx512_vpopcnt(const uint32_t* data, size_t size) {
-
     uint32_t ans = 0;
-    uint64_t start = (uint64_t) data;
-    if((start % 64) != 0) {
+    uint64_t start = (uint64_t)data;
+    if ((start % 64) != 0) {
         size_t rem = (start % 64) / 4;
         start = (start + 63) / 64 * 64;
         size -= rem;
-        FORI(i,rem) ans += popcount21(data[i]);
+        FORI(i, rem) ans += popcount21(data[i]);
     }
 
     const uint8_t* ptr = (uint8_t*)start;
@@ -145,8 +141,7 @@ uint32_t avx512_vpopcnt(const uint32_t* data, size_t size) {
 
     // count using AVX512 registers
     __m512i accumulator = _mm512_setzero_si512();
-    for (size_t i=0; i < chunks; i++, ptr += 64) {
-
+    for (size_t i = 0; i < chunks; i++, ptr += 64) {
         // Note: a short chain of dependencies, likely unrolling will be needed.
         const __m512i v = _mm512_loadu_si512((const __m512i*)ptr);
         const __m512i p = _mm512_popcnt_epi64(v);
@@ -158,7 +153,7 @@ uint32_t avx512_vpopcnt(const uint32_t* data, size_t size) {
     uint64_t tmp[8] __attribute__((aligned(64)));
     _mm512_store_si512((__m512i*)tmp, accumulator);
 
-    for (size_t i=0; i < 8; i++) {
+    for (size_t i = 0; i < 8; i++) {
         ans += (uint32_t)tmp[i];
     }
 
@@ -170,6 +165,7 @@ uint32_t avx512_vpopcnt(const uint32_t* data, size_t size) {
 
     return ans;
 }
+#endif
 
 void Bench(std::vector<uint32_t> values, int level) {
     auto t = Timer();
@@ -182,48 +178,54 @@ void Bench(std::vector<uint32_t> values, int level) {
     t.start();
     uint32_t ans = 0;
     if (level == 0) {
-        FORI(k,times) {
-            FORV(i,values,1) {
+        FORI(k, times) {
+            FORV(i, values, 1) {
                 uint32_t t = popcount11(values[i]);
                 ans += t;
             }
         }
-    } else if (level == 1)  {
-        FORI(k,times) {
-            FORV(i,values,1) {
+    } else if (level == 1) {
+        FORI(k, times) {
+            FORV(i, values, 1) {
                 uint32_t t = popcount21(values[i]);
                 ans += t;
             }
         }
     } else if (level == 2) {
-        FORI(k,times) {
-            FORV(i,values,2) {
-                uint32_t t = popcount22(values[i], values[i+1]);
+        FORI(k, times) {
+            FORV(i, values, 2) {
+                uint32_t t = popcount22(values[i], values[i + 1]);
                 ans += t;
             }
         }
     } else if (level == 4) {
-        FORI(k,times) {
-            FORV(i,values,4) {
-                uint32_t t = popcount24(values[i],values[i+1],values[i+2],values[i+3]);
+        FORI(k, times) {
+            FORV(i, values, 4) {
+                uint32_t t = popcount24(values[i], values[i + 1], values[i + 2],
+                                        values[i + 3]);
                 ans += t;
             }
         }
-    } else if (level == 8) {
-        const uint32_t* data = values.data();
-        FORI(k,times) {
-            ans += avx512_vpopcnt(data, values.size());
-        }
     }
+
+    #ifdef USE_AVX512_POPCNT
+    else if (level == 8) {
+        const uint32_t* data = values.data();
+        FORI(k, times) { ans += avx512_vpopcnt(data, values.size()); }
+    }
+    #endif
+
     t.stop();
     auto total = t.elapsedMilliseconds();
-    std::cout << "[" << name <<"] N = " << N << ", took: " << total << "ms, avg " << (total * 1e3) / N << "ns/N, ans = " << ans << "\n";
+    std::cout << "[" << name << "] N = " << N << ", took: " << total
+              << "ms, avg " << (total * 1e3) / N << "ns/N, ans = " << ans
+              << "\n";
 }
 
 void Test() {
     std::vector<uint32_t> values = GenRandomInts(100);
 
-    for(size_t i=0;i<values.size();i++) {
+    for (size_t i = 0; i < values.size(); i++) {
         uint32_t x = values[i];
         uint32_t a = popcount11(x);
         uint32_t b = popcount21(x);
@@ -232,34 +234,34 @@ void Test() {
 
     {
         uint32_t tta = 0, ttb = 0, ttc = 0;
-        for(size_t i=0;i<values.size();i++) {
+        for (size_t i = 0; i < values.size(); i++) {
             uint32_t x = values[i];
             uint32_t a = popcount21(x);
             tta += a;
         }
-        for(size_t i=0;i<values.size();i+=2) {
-            uint32_t b = popcount22(values[i], values[i+1]);
+        for (size_t i = 0; i < values.size(); i += 2) {
+            uint32_t b = popcount22(values[i], values[i + 1]);
             ttb += b;
         }
-        for(size_t i=0;i<values.size();i+=4) {
-            uint32_t c = popcount24(values[i], values[i+1], values[i+2], values[i+3]);
+        for (size_t i = 0; i < values.size(); i += 4) {
+            uint32_t c = popcount24(values[i], values[i + 1], values[i + 2],
+                                    values[i + 3]);
             ttc += c;
         }
         printf("tta = %u, ttb = %u, ttc = %u\n", tta, ttb, ttc);
     }
 
-    return ;
+    return;
 }
 
-int main()
-{
+int main() {
     // Test();
     const int N = 1000;
     std::vector<uint32_t> values = GenRandomInts(N);
-    const int levels[] = {0,1,2,4};
+    const int levels[] = {0, 1, 2, 4};
     const int nums = sizeof(levels) / sizeof(levels[0]);
-    for(int i=0;i<nums;i++) {
+    for (int i = 0; i < nums; i++) {
         Bench(values, levels[i]);
     }
-	return 0;
+    return 0;
 }
