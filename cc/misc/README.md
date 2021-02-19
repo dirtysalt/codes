446bd9cf5469edc6a4e2520b4c3b14a92cee90dc
yanz <dirtysalt1987@gmail.com>
2021-02-19 20:59:06 +0800

    update

--- cc/misc/README.md
+++
@@ -1,8 +0,0 @@
-- `ob_math_pic.cc` 生成分形 [图形](./ob_math_pic.jpg)
-- `runable_so.cc` 生成可执行.so文件
-- `test_pye.cc` 用boost编写python extension
-- `xorll.cc` XOR 链表
-- `FloatTest.c` 一些浮点和整数时间转换操作
-- `MatrixTranpose.cpp` 矩阵转置的cache优化
-- `PopCountTest.cpp` 计算popcount的几种方法
-- `opt_linear_search.cc` 循环展开对搜索线性搜索的影响
---
+++ cc/misc/primes.go
@@ -0,0 +1,52 @@
+// coding:utf-8
+// Copyright (C) dirlt
+
+package main
+
+import "fmt"
+
+func P(left chan int, output chan int, done chan bool) {
+	p := <-left
+	output <- p
+
+	var right chan int
+	var rdone chan bool
+
+	for x := range left {
+		if (x % p) == 0 {
+			continue
+		}
+		if right == nil {
+			right = make(chan int)
+			rdone = make(chan bool)
+			go P(right, output, rdone)
+		}
+		right <- x
+	}
+	if right != nil {
+		close(right)
+		<-rdone
+	}
+	done <- true
+}
+
+func collect(output chan int) {
+	for p := range output {
+		fmt.Printf("prime number: %d\n", p)
+	}
+}
+
+func main() {
+	output := make(chan int)
+	input := make(chan int)
+	done := make(chan bool)
+	const N = 100
+	go P(input, output, done)
+	go collect(output)
+	for i := 2; i < N; i++ {
+		input <- i
+	}
+	close(input)
+	<-done
+	close(output)
+}
