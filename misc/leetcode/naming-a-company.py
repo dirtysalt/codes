caad1a12c21b31685e6a65d4ba977ed9147922d9
yanz <dirtysalt1987@gmail.com>
2022-06-12 15:40:18 +0800

    update files

--- misc/leetcode/naming-a-company.py
+++ misc/leetcode/naming-a-company.py
@@ -7,6 +7,24 @@ from typing import List
 
 class Solution:
     def distinctNames(self, ideas: List[str]) -> int:
+        ss = set(ideas)
+        cnt = [[0] * 26 for _ in range(26)]
+        for s in ideas:
+            c = ord(s[0]) - ord('a')
+            for i in range(26):
+                s2 = chr(i + ord('a')) + s[1:]
+                if s2 not in ss:
+                    cnt[c][i] += 1
+
+        ans = 0
+        for i in range(26):
+            for j in range(26):
+                ans += cnt[i][j] * cnt[j][i]
+        return ans
+
+
+class Solution:
+    def distinctNames(self, ideas: List[str]) -> int:
         ss = [set() for _ in range(26)]
         for s in ideas:
             c = ord(s[0]) - ord('a')
