a7fd62a0215bca98db1e6b0e787b3826e9f9042c
yanz <dirtysalt1987@gmail.com>
2023-07-09 21:26:46 +0800

    update files
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- misc/leetcode/apply-operations-to-make-all-array-elements-equal-to-zero.py
+++ misc/leetcode/apply-operations-to-make-all-array-elements-equal-to-zero.py
@@ -10,7 +10,7 @@ import heapq
 
 class Solution:
     def checkArray(self, nums: List[int], k: int) -> bool:
-        nums.extend([0] * k)
+        if k == 1: return True
         n = len(nums)
         C = [0] * (n + k)
 
@@ -21,6 +21,8 @@ class Solution:
             if v < 0: return False
             C[i + k] = v
             c += v
+
+        if sum(C[-(k - 1):]) != 0: return False
         return True
 
 
@@ -28,9 +30,9 @@ true, false, null = True, False, None
 import aatest_helper
 
 cases = [
-    ([2, 2, 3, 1, 1, 0], 3, true),
-    ([1, 3, 1, 1], 2, false),
-    ([0, 45, 82, 98, 99], 4, false)
+    ([0, 45, 82, 98, 99], 4, false),
+    ([60, 72, 87, 89, 63, 52, 64, 62, 31, 37, 57, 83, 98, 94, 92, 77, 94, 91, 87, 100, 91, 91, 50, 26], 4, true),
+    ([63, 40, 30, 0, 72, 53], 1, true),
 ]
 
 aatest_helper.run_test_cases(Solution().checkArray, cases)
