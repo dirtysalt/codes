5d6e49c10923f505df9ecbaea9bc1b5bd22a75ef
yanz <dirtysalt1987@gmail.com>
2023-07-29 15:24:46 +0800

    add libhdfs perf tools

---
+++ cc/sr-test/run_libhdfs_perf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export STARROCKS_HOME=${STARROCKS_HOME}/output/be
+export HADOOP_CLASSPATH=${STARROCKS_HOME}/lib/hadoop/common/*:${STARROCKS_HOME}/lib/hadoop/common/lib/*:${STARROCKS_HOME}/lib/hadoop/hdfs/*:${STARROCKS_HOME}/lib/hadoop/hdfs/lib/*
+export CLASSPATH=$STARROCKS_HOME/conf:$HADOOP_CLASSPATH:$CLASSPATH
+./libhdfs_perf.exe --endpoint hdfs://172.26.194.238:9000 --path /user/zya/tpch_100g/orc/zlib/lineitem/
