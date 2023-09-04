52208cca9676a08395cc13735364d7b3b6cfa576
yanz <dirtysalt1987@gmail.com>
2023-09-04 14:41:45 +0800

    update files

--- misc/leetcode/aadatastruct.py
+++ misc/leetcode/aadatastruct.py
@@ -294,5 +294,59 @@ class RangeTree:
         return search(1, 0, self.n)
 
 
+def tarjan_lca(graph, root, queries):
+    class UnionFindSet:
+        def __init__(self, n):
+            self.ps = [0] * n
+            for i in range(n):
+                self.ps[i] = i
+
+        def find(self, x):
+            p = x
+            while self.ps[p] != p:
+                p = self.ps[p]
+
+            while self.ps[x] != x:
+                up = self.ps[x]
+                self.ps[x] = p
+                x = up
+            return p
+
+        def set(self, x, p):
+            self.ps[x] = p
+
+    from collections import defaultdict
+    query_index = defaultdict(list)
+    ans = [-1] * len(queries)
+    for idx, (u, v) in enumerate(queries):
+        query_index[u].append((v, idx))
+        query_index[v].append((u, idx))
+
+    n = len(graph)
+    ufs = UnionFindSet(n)
+
+    def dfs(root, parent):
+        # answer queries.
+        query = query_index[root]
+        for v, idx in query:
+            # 如果有对应的查询节点v, 并且这个节点之前访问过
+            # 那么使用这个节点的parent.
+            # 如果v是root的祖先节点的话，那么就是v
+            # 如果v在另外一个树上的话，那么就是最早交汇的节点
+            p = ufs.find(v)
+            if p != -1:
+                ans[idx] = p
+
+        # continue to dfs.
+        for v, _ in graph[root]:
+            if v != parent:
+                dfs(v, root)
+                # 遍历子节点之后，将子节点的父节点设置为自己
+                ufs.set(v, root)
+
+    dfs(root, -1)
+    return ans
+
+
 if __name__ == '__main__':
     pass
