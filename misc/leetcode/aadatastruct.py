9649fd9e42d63d1ff6e1f4decebd381c13dc887f
yanz <dirtysalt1987@gmail.com>
2024-01-17 07:01:31 +0800

    fix compilation

--- misc/leetcode/aadatastruct.py
+++ misc/leetcode/aadatastruct.py
@@ -484,5 +484,53 @@ def mat_mul(a, b, MOD):
     return res
 
 
+class KMP:
+    @staticmethod
+    def build_max_match(t):
+        n = len(t)
+        match = [0] * n
+        c = 0
+        for i in range(1, n):
+            v = t[i]
+            while c and t[c] != v:
+                c = match[c - 1]
+            if t[c] == v:
+                c += 1
+            match[i] = c
+        return match
+
+    def __init__(self, t):
+        self.t = t
+        self.max_match = self.build_max_match(t)
+
+    def search(self, s):
+        match = self.max_match
+        t = self.t
+        c = 0
+        for i, v in enumerate(s):
+            while c and t[c] != v:
+                c = match[c - 1]
+            if t[c] == v:
+                c += 1
+            if c == len(t):
+                return i - len(t) + 1
+        return -1
+
+    def find_all(self, s):
+        match = self.max_match
+        t = self.t
+        c = 0
+        pos = []
+        for i, v in enumerate(s):
+            while c and t[c] != v:
+                c = match[c - 1]
+            if t[c] == v:
+                c += 1
+            if c == len(t):
+                pos.append(i - len(t) + 1)
+                c = match[c - 1]
+        return pos
+
+
 if __name__ == '__main__':
     pass
