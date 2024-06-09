ae77f4d9092e4b582906945c8af13c99de1d2e81
yanz <dirtysalt1987@gmail.com>
2024-06-09 14:20:21 -0700

    update files

--- misc/leetcode/find-the-maximum-length-of-a-good-subsequence-i.py
+++ misc/leetcode/find-the-maximum-length-of-a-good-subsequence-i.py
@@ -5,74 +5,45 @@
 from typing import List
 
 
-class State:
-    def __init__(self):
-        self.values = {}
-        self.sizes = []
-
-    def check(self, v):
-        if v not in self.values:
-            self.values[v] = 0
-            self.sizes.append((0, v))
-
-    def update(self, v, sz):
-        self.check(v)
-        prev = self.values[v]
-        if sz > prev:
-            self.values[v] = sz
-            self.sizes.append((sz, v))
-            self.fix_sizes()
-
-    def fix_sizes(self):
-        self.sizes.sort(reverse=True)
-        tmp = []
-        for sz2, v2 in self.sizes:
-            if any(x[1] == v2 for x in tmp): continue
-            tmp.append((sz2, v2))
-            if len(tmp) == 2: break
-        self.sizes = tmp
-
-    def query(self, v):
-        self.check(v)
-        sz = self.values[v]
-        next_sz = 0
-        for sz2, v2 in self.sizes:
-            if v == v2: continue
-            next_sz = sz2
-            break
-        return sz, next_sz
-
-    def max_size(self):
-        return self.sizes[0][0]
-
-    def __repr__(self):
-        return f"values = {self.values}, sizes = {self.sizes}"
-
-
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
-        states = [State() for _ in range(k + 1)]
-        for x in nums:
-            for j in reversed(range(k + 1)):
-                sz, next_sz = states[j].query(x)
-                states[j].update(x, sz + 1)
-                if j < k:
-                    states[j + 1].update(x, next_sz + 1)
+        mx = [[0] * 3 for _ in range(k + 1)]
+        fx = {}
 
-        ans = 0
-        for j in range(0, k + 1):
-            ans = max(ans, states[j].max_size())
-        return ans
+        for x in nums:
+            if x not in fx:
+                fx[x] = [0] * (k + 1)
+            f = fx[x]
+
+            for kk in range(k, -1, -1):
+                f[kk] += 1
+                if kk > 0:
+                    m = mx[kk - 1]
+                    f[kk] = max(f[kk], (m[1] if m[2] == x else m[0]) + 1)
+                    # f[kk] = max(f[kk], m[0] + 1)
+
+                # update m
+                v = f[kk]
+                m = mx[kk]
+                if v > m[0]:
+                    if m[2] == x:
+                        m[0] = v
+                    else:
+                        m[0], m[1], m[2] = v, m[0], x
+                elif v > m[1] and m[2] != x:
+                    m[1] = v
+
+            # print(mx)
+
+        return mx[k][0]
 
 
 true, false, null = True, False, None
 import aatest_helper
 
 cases = [
-    aatest_helper.OrderedDict(nums=[1, 2, 1, 1, 3], k=2, res=4),
-    aatest_helper.OrderedDict(nums=[1, 2, 3, 4, 5, 1], k=0, res=2),
-    ([2, 5], 1, 2),
-    ([29, 29, 28], 0, 2)
+    ([1, 2, 1, 1, 3], 2, 4),
+    ([1, 2, 3, 4, 5, 1], 0, 2),
 ]
 
 aatest_helper.run_test_cases(Solution().maximumLength, cases)
