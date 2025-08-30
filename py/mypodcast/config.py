17b45d989087eff6f3aa38f024698d46f333f35e
yanz <dirtysalt1987@gmail.com>
2025-08-30 09:19:02 +0800

    update files
    Signed-off-by: yanz <dirtysalt1987@gmail.com>

--- py/mypodcast/config.py
+++ py/mypodcast/config.py
@@ -2,29 +2,24 @@
 # coding:utf-8
 # Copyright (C) dirlt
 
-
-sites = {
-    'A_Dream_in_Red_Mansions': {
-        'name': 'A_Dream_in_Red_Mansions',
-        'title': '蒋勋细说红楼梦',
-    },    
-}
-
 sites = {  
     'RAZ_B_Podcast': {
         'name': 'RAZ_B_Podcast',
         'title': 'RAZ B级别音频 5集连播',
-    }
-}
-
-sites = {
-    'RAZ_C_Podcast': {
+    },
+     'RAZ_C_Podcast': {
         'name': 'RAZ_C_Podcast',
         'title': 'RAZ C级别音频 5集连播',
     },
+     'A_Dream_in_Red_Mansions': {
+        'name': 'A_Dream_in_Red_Mansions',
+        'title': '蒋勋细说红楼梦',
+    },
 }
 
+
 glob = {
+    # change this if you want to publish it to other place.
     'domain': '192.168.1.8:18000/mypodcast',
     'author': 'dirtysalt',
     'link': 'https://dirtysalt.github.io',
